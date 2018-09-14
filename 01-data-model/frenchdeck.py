25f1c81fa0e81b7396df0940bae123703e009423
kernellmd <KernelLmd@163.com>
2018-09-14 11:43:02 +0800

    modify frenchdeck.py

--- 01-data-model/frenchdeck.py
+++ 01-data-model/frenchdeck.py
@@ -1,3 +1,9 @@
+"""
+扑克牌的正确叫法应该是英美游戏牌（Anglo-American Playing Card）
+或是法国牌组（French Deck），扑克（Poker）只是其中一种玩法，
+由于进入中国时这种玩法最为流行，也连带着把这种纸牌称作扑克牌。
+"""
+
 import collections
 
 Card = collections.namedtuple('Card', ['rank', 'suit'])
@@ -13,5 +19,6 @@ class FrenchDeck:
     def __len__(self):
         return len(self._cards)
 
+    #仅实现__getitem__方法，就可使对象可迭代
     def __getitem__(self, position):
         return self._cards[position]
